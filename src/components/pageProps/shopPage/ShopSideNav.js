4ba3426e764dd2bd189e13a5fca62a26d92ab5e2
sofianeFeki <backofficekompar@gmail.com>
2023-12-12 17:36:42 +0100

    test

--- src/components/pageProps/shopPage/ShopSideNav.js
+++ src/components/pageProps/shopPage/ShopSideNav.js
@@ -4,52 +4,6 @@ import Category from "./shopBy/Category";
 import Color from "./shopBy/Color";
 //import Price from "./shopBy/Price";
 
-function Items({
-  currentItems,
-  selectedBrands,
-  selectedCategories,
-  selectedColors,
-}) {
-  // Filter items based on selected brands and categories
-  const filteredItems = currentItems.filter((item) => {
-    const isBrandSelected =
-      selectedBrands.length === 0 ||
-      selectedBrands.some((brand) => brand.title === item.brand);
-
-    const isCategorySelected =
-      selectedCategories.length === 0 ||
-      selectedCategories.some((category) => category.title === item.cat);
-
-    const isColorSelected =
-      isCategorySelected && item.cat === "Encre"
-        ? selectedColors.length === 0 ||
-          selectedColors.some((color) => color.title === item.color)
-        : true;
-
-    return isBrandSelected && isCategorySelected && isColorSelected;
-  });
-
-  return (
-    <>
-      {filteredItems.map((item) => (
-        <div key={item._id} className="w-full">
-          <Product
-            _id={item._id}
-            img={item.img}
-            productName={item.productName}
-            price={item.price}
-            color={item.color}
-            badge={item.badge}
-            des={item.des}
-            pdf={item.pdf}
-            ficheTech={item.ficheTech}
-          />
-        </div>
-      ))}
-    </>
-  );
-}
-
 const ShopSideNav = () => {
   return (
     <div className="w-full flex flex-col gap-6">
