30da054f84be4a2ed335ce1d5b8c959933dde392
sofianeFeki <backofficekompar@gmail.com>
2024-01-20 18:01:19 +0100

    1.1.3

--- src/assets/images/index.js
+++ src/assets/images/index.js
@@ -167,6 +167,159 @@ import CTL_CAN_FX10 from "../images/products/toner/canon/CTL_CAN_FX10.png";
 import CTL_CAN_LBP703 from "../images/products/toner/canon/CTL_CAN_LBP703.png";
 import CTL_CAN_PG40 from "../images/products/toner/canon/CTL_CAN_PG40.png";
 
+import CTD_HP_1VV21AE from "../images/products/cartouche/CTD_HP_1VV21AE.png";
+import CTD_HP_3YM60AE from "../images/products/cartouche/CTD_HP_3YM60AE.png";
+import CTD_HP_3YM61AE from "../images/products/cartouche/CTD_HP_3YM61AE.png";
+import CTD_HP_3YP61AE from "../images/products/cartouche/CTD_HP_3YP61AE.png";
+import CTD_HP_6ZA11AE from "../images/products/cartouche/CTD_HP_6ZA11AE.png";
+import CTD_HP_6ZA17AE from "../images/products/cartouche/CTD_HP_6ZA17AE.png";
+import CTD_HP_6ZA18AE from "../images/products/cartouche/CTD_HP_6ZA18AE.png";
+import CTD_HP_6ZC65AE from "../images/products/cartouche/CTD_HP_6ZC65AE.png";
+import CTD_HP_B3P19A from "../images/products/cartouche/CTD_HP_B3P19A.png";
+import CTD_HP_B3P20A from "../images/products/cartouche/CTD_HP_B3P20A.png";
+import CTD_HP_B3P21A from "../images/products/cartouche/CTD_HP_B3P21A.png";
+import CTD_HP_B3P23A from "../images/products/cartouche/CTD_HP_B3P23A.png";
+import CTD_HP_B3P24A from "../images/products/cartouche/CTD_HP_B3P24A.png";
+import CTD_HP_C51645A from "../images/products/cartouche/CTD_HP_C51645A.png";
+import CTD_HP_C6578DE from "../images/products/cartouche/CTD_HP_C6578DE.png";
+import CTD_HP_C8766HE from "../images/products/cartouche/CTD_HP_C8766HE.png";
+import CTD_HP_C8767HE from "../images/products/cartouche/CTD_HP_C8767HE.png";
+import CTD_HP_C9364EE from "../images/products/cartouche/CTD_HP_C9364EE.png";
+import CTD_HP_CC640HE from "../images/products/cartouche/CTD_HP_CC640HE.png";
+import CTD_HP_CD972A from "../images/products/cartouche/CTD_HP_CD972A.png";
+import CTD_HP_CD973A from "../images/products/cartouche/CTD_HP_CD973A.png";
+import CTD_HP_CD974A from "../images/products/cartouche/CTD_HP_CD974A.png";
+import CTD_HP_CD975A from "../images/products/cartouche/CTD_HP_CD975A.png";
+import CTD_HP_CH561HE from "../images/products/cartouche/CTD_HP_CH561HE.png";
+import CTD_HP_CH562HE from "../images/products/cartouche/CTD_HP_CH562HE.png";
+import CTD_HP_CN046AE from "../images/products/cartouche/CTD_HP_CN046AE.png";
+import CTD_HP_CN047AE from "../images/products/cartouche/CTD_HP_CN047AE.png";
+import CTD_HP_CN048AE from "../images/products/cartouche/CTD_HP_CN048AE.png";
+import CTD_HP_CN049AE from "../images/products/cartouche/CTD_HP_CN049AE.png";
+import CTD_HP_CN050AE from "../images/products/cartouche/CTD_HP_CN050AE.png";
+import CTD_HP_CN051AE from "../images/products/cartouche/CTD_HP_CN051AE.png";
+import CTD_HP_CN052AE from "../images/products/cartouche/CTD_HP_CN052AE.png";
+import CTD_HP_CN053AE from "../images/products/cartouche/CTD_HP_CN053AE.png";
+import CTD_HP_CN054AE from "../images/products/cartouche/CTD_HP_CN054AE.png";
+import CTD_HP_CN055AE from "../images/products/cartouche/CTD_HP_CN055AE.png";
+import CTD_HP_CN056AE from "../images/products/cartouche/CTD_HP_CN056AE.png";
+import CTD_HP_CN057AE from "../images/products/cartouche/CTD_HP_CN057AE.png";
+import CTD_HP_CZ101A from "../images/products/cartouche/CTD_HP_CZ101A.png";
+import CTD_HP_CZ102A from "../images/products/cartouche/CTD_HP_CZ102A.png";
+import CTD_HP_F6U12AE from "../images/products/cartouche/CTD_HP_F6U12AE.png";
+import CTD_HP_F6U13AE from "../images/products/cartouche/CTD_HP_F6U13AE.png";
+import CTD_HP_F6U14AE from "../images/products/cartouche/CTD_HP_F6U14AE.png";
+import CTD_HP_F6V16AE from "../images/products/cartouche/CTD_HP_F6V16AE.png";
+import CTD_HP_L0S58AE from "../images/products/cartouche/CTD_HP_L0S58AE.png";
+import CTD_HP_M0H54AE from "../images/products/cartouche/CTD_HP_M0H54AE.png";
+import CTD_HP_M0H55AE from "../images/products/cartouche/CTD_HP_M0H55AE.png";
+import CTD_HP_M0H56AE from "../images/products/cartouche/CTD_HP_M0H56AE.png";
+import CTD_HP_N9K05AE from "../images/products/cartouche/CTD_HP_N9K05AE.png";
+import CTL_HP_C6511 from "../images/products/cartouche/CTL_HP_C6511.png";
+import CTL_HP_C7115A_AD from "../images/products/cartouche/CTL_HP_C7115A_AD.png";
+import CTL_HP_CB435A from "../images/products/cartouche/CTL_HP_CB435A.png";
+import CTL_HP_CB540A from "../images/products/cartouche/CTL_HP_CB540A.png";
+import CTL_HP_CB542A from "../images/products/cartouche/CTL_HP_CB542A.png";
+import CTL_HP_CB543A from "../images/products/cartouche/CTL_HP_CB543A.png";
+import CTL_HP_CE255A from "../images/products/cartouche/CTL_HP_CE255A.png";
+import CTL_HP_CE255X from "../images/products/cartouche/CTL_HP_CE255X.png";
+import CTL_HP_CE278A from "../images/products/cartouche/CTL_HP_CE278A.png";
+import CTL_HP_CE285A from "../images/products/cartouche/CTL_HP_CE285A.png";
+import CTL_HP_CE310A from "../images/products/cartouche/CTL_HP_CE310A.png";
+import CTL_HP_CE311A from "../images/products/cartouche/CTL_HP_CE311A.png";
+import CTL_HP_CE312A from "../images/products/cartouche/CTL_HP_CE312A.png";
+import CTL_HP_CE313A from "../images/products/cartouche/CTL_HP_CE313A.png";
+import CTL_HP_CE320A from "../images/products/cartouche/CTL_HP_CE320A.png";
+import CTL_HP_CE321A from "../images/products/cartouche/CTL_HP_CE321A.png";
+import CTL_HP_CE322A from "../images/products/cartouche/CTL_HP_CE322A.png";
+import CTL_HP_CE323A from "../images/products/cartouche/CTL_HP_CE323A.png";
+import CTL_HP_CE390A from "../images/products/cartouche/CTL_HP_CE390A.png";
+import CTL_HP_CE400A from "../images/products/cartouche/CTL_HP_CE400A.png";
+import CTL_HP_CE401A from "../images/products/cartouche/CTL_HP_CE401A.png";
+import CTL_HP_CE402A from "../images/products/cartouche/CTL_HP_CE402A.png";
+import CTL_HP_CE403A from "../images/products/cartouche/CTL_HP_CE403A.png";
+import CTL_HP_CE410A from "../images/products/cartouche/CTL_HP_CE410A.png";
+import CTL_HP_CE410X from "../images/products/cartouche/CTL_HP_CE410X.png";
+import CTL_HP_CE411A from "../images/products/cartouche/CTL_HP_CE411A.png";
+import CTL_HP_CE412A from "../images/products/cartouche/CTL_HP_CE412A.png";
+import CTL_HP_CE413A from "../images/products/cartouche/CTL_HP_CE413A.png";
+import CTL_HP_CE505A from "../images/products/cartouche/CTL_HP_CE505A.png";
+import CTL_HP_CE505XD from "../images/products/cartouche/CTL_HP_CE505XD.png";
+import CTL_HP_CE740A from "../images/products/cartouche/CTL_HP_CE740A.png";
+import CTL_HP_CE741A from "../images/products/cartouche/CTL_HP_CE741A.png";
+import CTL_HP_CE742A from "../images/products/cartouche/CTL_HP_CE742A.png";
+import CTL_HP_CE743A from "../images/products/cartouche/CTL_HP_CE743A.png";
+import CTL_HP_CF210A from "../images/products/cartouche/CTL_HP_CF210A.png";
+import CTL_HP_CF211A from "../images/products/cartouche/CTL_HP_CF211A.png";
+import CTL_HP_CF212A from "../images/products/cartouche/CTL_HP_CF212A.png";
+import CTL_HP_CF213A from "../images/products/cartouche/CTL_HP_CF213A.png";
+import CTL_HP_CF217 from "../images/products/cartouche/CTL_HP_CF217.png";
+import CTL_HP_CF226A from "../images/products/cartouche/CTL_HP_CF226A.png";
+import CTL_HP_CF226X from "../images/products/cartouche/CTL_HP_CF226X.png";
+import CTL_HP_CF230A from "../images/products/cartouche/CTL_HP_CF230A.png";
+import CTL_HP_CF244A from "../images/products/cartouche/CTL_HP_CF244A.png";
+import CTL_HP_CF259A from "../images/products/cartouche/CTL_HP_CF259A.png";
+import CTL_HP_CF259X from "../images/products/cartouche/CTL_HP_CF259X.png";
+import CTL_HP_CF280A from "../images/products/cartouche/CTL_HP_CF280A.png";
+import CTL_HP_CF280X from "../images/products/cartouche/CTL_HP_CF280X.png";
+import CTL_HP_CF281A from "../images/products/cartouche/CTL_HP_CF281A.png";
+import CTL_HP_CF283A from "../images/products/cartouche/CTL_HP_CF283A.png";
+import CTL_HP_CF289A from "../images/products/cartouche/CTL_HP_CF289A.png";
+import CTL_HP_CF360A from "../images/products/cartouche/CTL_HP_CF360A.png";
+import CTL_HP_CF360X from "../images/products/cartouche/CTL_HP_CF360X.png";
+import CTL_HP_CF361A from "../images/products/cartouche/CTL_HP_CF361A.png";
+import CTL_HP_CF361X from "../images/products/cartouche/CTL_HP_CF361X.png";
+import CTL_HP_CF362A from "../images/products/cartouche/CTL_HP_CF362A.png";
+import CTL_HP_CF362X from "../images/products/cartouche/CTL_HP_CF362X.png";
+import CTL_HP_CF363A from "../images/products/cartouche/CTL_HP_CF363A.png";
+import CTL_HP_CF363X from "../images/products/cartouche/CTL_HP_CF363X.png";
+import CTL_HP_CF400A from "../images/products/cartouche/CTL_HP_CF400A.png";
+import CTL_HP_CF401A from "../images/products/cartouche/CTL_HP_CF401A.png";
+import CTL_HP_CF402A from "../images/products/cartouche/CTL_HP_CF402A.png";
+import CTL_HP_CF403A from "../images/products/cartouche/CTL_HP_CF403A.png";
+import CTL_HP_CF410A from "../images/products/cartouche/CTL_HP_CF410A.png";
+import CTL_HP_CF410X from "../images/products/cartouche/CTL_HP_CF410X.png";
+import CTL_HP_CF411A from "../images/products/cartouche/CTL_HP_CF411A.png";
+import CTL_HP_CF411X from "../images/products/cartouche/CTL_HP_CF411X.png";
+import CTL_HP_CF412A from "../images/products/cartouche/CTL_HP_CF412A.png";
+import CTL_HP_CF412X from "../images/products/cartouche/CTL_HP_CF412X.png";
+import CTL_HP_CF413A from "../images/products/cartouche/CTL_HP_CF413A.png";
+import CTL_HP_CF413X from "../images/products/cartouche/CTL_HP_CF413X.png";
+import CTL_HP_CF530A from "../images/products/cartouche/CTL_HP_CF530A.png";
+import CTL_HP_CF531A from "../images/products/cartouche/CTL_HP_CF531A.png";
+import CTL_HP_CF532A from "../images/products/cartouche/CTL_HP_CF532A.png";
+import CTL_HP_CF533A from "../images/products/cartouche/CTL_HP_CF533A.png";
+import CTL_HP_CF540A from "../images/products/cartouche/CTL_HP_CF540A.png";
+import CTL_HP_CF541A from "../images/products/cartouche/CTL_HP_CF541A.png";
+import CTL_HP_CF541X from "../images/products/cartouche/CTL_HP_CF541X.png";
+import CTL_HP_CF542X from "../images/products/cartouche/CTL_HP_CF542X.png";
+import CTL_HP_CF543X from "../images/products/cartouche/CTL_HP_CF543X.png";
+import CTL_HP_Q2612A from "../images/products/cartouche/CTL_HP_Q2612A.png";
+import CTL_HP_Q2624A from "../images/products/cartouche/CTL_HP_Q2624A.png";
+import CTL_HP_Q5949A from "../images/products/cartouche/CTL_HP_Q5949A.png";
+import CTL_HP_Q5952AC from "../images/products/cartouche/CTL_HP_Q5952AC.png";
+import CTL_HP_Q5953A from "../images/products/cartouche/CTL_HP_Q5953A.png";
+import CTL_HP_Q7551A from "../images/products/cartouche/CTL_HP_Q7551A.png";
+import CTL_HP_Q7551X from "../images/products/cartouche/CTL_HP_Q7551X.png";
+import CTL_HP_Q7553A from "../images/products/cartouche/CTL_HP_Q7553A.png";
+import CTL_HP_W1106A from "../images/products/cartouche/CTL_HP_W1106A.png";
+import CTL_HP_W1335A from "../images/products/cartouche/CTL_HP_W1335A.png";
+import CTL_HP_W1360A from "../images/products/cartouche/CTL_HP_W1360A.png";
+import CTL_HP_W1500A from "../images/products/cartouche/CTL_HP_W1500A.png";
+import CTL_HP_W2030X from "../images/products/cartouche/CTL_HP_W2030X.png";
+import CTL_HP_W2031A from "../images/products/cartouche/CTL_HP_W2031A.png";
+import CTL_HP_W2032A from "../images/products/cartouche/CTL_HP_W2032A.png";
+import CTL_HP_W2033A from "../images/products/cartouche/CTL_HP_W2033A.png";
+import CTL_HP_W2071A from "../images/products/cartouche/CTL_HP_W2071A.png";
+import CTL_HP_W2210A from "../images/products/cartouche/CTL_HP_W2210A.png";
+import CTL_HP_W2211A from "../images/products/cartouche/CTL_HP_W2211A.png";
+import CTL_HP_W2212A from "../images/products/cartouche/CTL_HP_W2212A.png";
+import CTL_HP_W2213A from "../images/products/cartouche/CTL_HP_W2213A.png";
+import CTL_HP_W2410A from "../images/products/cartouche/CTL_HP_W2410A.png";
+import CTL_HP_W2411A from "../images/products/cartouche/CTL_HP_W2411A.png";
+import CTL_HP_W2412A from "../images/products/cartouche/CTL_HP_W2412A.png";
+import CTL_HP_W2413A from "../images/products/cartouche/CTL_HP_W2413A.png";
+
 import CTD_BRO_LC121BK from "../images/products/toner/brother/CTD_BRO_LC121BK.png";
 import CTD_BRO_LC121C from "../images/products/toner/brother/CTD_BRO_LC121C.png";
 import CTD_BRO_LC121M from "../images/products/toner/brother/CTD_BRO_LC121M.png";
@@ -181,6 +334,30 @@ import CTL_BRO_TN325M from "../images/products/toner/brother/CTL_BRO_TN325M.png"
 import CTL_BRO_TN325Y from "../images/products/toner/brother/CTL_BRO_TN325Y.png";
 import CTL_BRO_TN2220 from "../images/products/toner/brother/CTL_BRO_TN2220.png";
 import CTL_BRO_TN2320 from "../images/products/toner/brother/CTL_BRO_TN2320.png";
+
+import RB_BRO_PC74 from "../images/products/ruban/RB_BRO_PC74.png";
+import RB_BRO_TZ231 from "../images/products/ruban/RB_BRO_TZ231.png";
+import RB_COM_PRK6287 from "../images/products/ruban/RB_COM_PRK6287.png";
+import RB_COMP_PRKN102_1 from "../images/products/ruban/RB_COMP_PRKN102-1.png";
+import RB_EPS_S015336BA from "../images/products/ruban/RB_EPS_S015336BA.png";
+import RB_EPS_SO15086 from "../images/products/ruban/RB_EPS_SO15086.png";
+import RB_EPS_SO15329 from "../images/products/ruban/RB_EPS_SO15329.png";
+import RB_EPS_SO15337 from "../images/products/ruban/RB_EPS_SO15337.png";
+import RB_FUJ_CA02374C104 from "../images/products/ruban/RB_FUJ_CA02374C104.png";
+import RB_LEX_3070166 from "../images/products/ruban/RB_LEX_3070166.png";
+import RB_OLI_B0375 from "../images/products/ruban/RB_OLI_B0375.png";
+import RB_PRI_255049101_2 from "../images/products/ruban/RB_PRI_255049101 (2).png";
+import RB_PRI_255049101 from "../images/products/ruban/RB_PRI_255049101.png";
+import RB_TAL_062471 from "../images/products/ruban/RB_TAL_062471.png";
+import RB_TAL_2610 from "../images/products/ruban/RB_TAL_2610+.png";
+import RB_TAL_T5040 from "../images/products/ruban/RB_TAL_T5040.png";
+import RB_EPS_SO15637BA from "../images/products/ruban/RB_EPS_SO15637BA.png";
+
+import BAC_LEX_78C0W00 from "../images/products/bac/BAC_LEX_78C0W00.png";
+import BAC_RIC_405783 from "../images/products/bac/BAC_RIC_405783.png";
+import BAC_RIC_TK1220 from "../images/products/bac/BAC_RIC_TK1220.png";
+import BAC_XER_008R13061 from "../images/products/bac/BAC_XER_008R13061.png";
+import BAC_XER_108R01416 from "../images/products/bac/BAC_XER_108R01416.png";
 
 import IMPRIMANTE_PANTUM_CP2200DW from "../images/products/imprimante/IMPRIMANTE_PANTUM_CP2200DW.webp";
 import IMPRIMANTE_PANTUM_BM5100FDW from "../images/products/imprimante/IMPRIMANTE_PANTUM_BM5100FDW.webp";
@@ -451,4 +628,178 @@ export {
   CTL_BRO_TN325Y,
   CTL_BRO_TN2220,
   CTL_BRO_TN2320,
+  RB_BRO_PC74,
+  RB_BRO_TZ231,
+  RB_COM_PRK6287,
+  RB_COMP_PRKN102_1,
+  RB_EPS_S015336BA,
+  RB_EPS_SO15086,
+  RB_EPS_SO15329,
+  RB_EPS_SO15337,
+  RB_FUJ_CA02374C104,
+  RB_LEX_3070166,
+  RB_OLI_B0375,
+  RB_PRI_255049101_2,
+  RB_PRI_255049101,
+  RB_TAL_062471,
+  RB_TAL_2610,
+  RB_TAL_T5040,
+  RB_EPS_SO15637BA,
+  BAC_LEX_78C0W00,
+  BAC_RIC_405783,
+  BAC_RIC_TK1220,
+  BAC_XER_008R13061,
+  BAC_XER_108R01416,
+  CTD_HP_1VV21AE,
+  CTD_HP_3YM60AE,
+  CTD_HP_3YM61AE,
+  CTD_HP_3YP61AE,
+  CTD_HP_6ZA11AE,
+  CTD_HP_6ZA17AE,
+  CTD_HP_6ZA18AE,
+  CTD_HP_6ZC65AE,
+  CTD_HP_B3P19A,
+  CTD_HP_B3P20A,
+  CTD_HP_B3P21A,
+  CTD_HP_B3P23A,
+  CTD_HP_B3P24A,
+  CTD_HP_C51645A,
+  CTD_HP_C6578DE,
+  CTD_HP_C8766HE,
+  CTD_HP_C8767HE,
+  CTD_HP_C9364EE,
+  CTD_HP_CC640HE,
+  CTD_HP_CD972A,
+  CTD_HP_CD973A,
+  CTD_HP_CD974A,
+  CTD_HP_CD975A,
+  CTD_HP_CH561HE,
+  CTD_HP_CH562HE,
+  CTD_HP_CN046AE,
+  CTD_HP_CN047AE,
+  CTD_HP_CN048AE,
+  CTD_HP_CN049AE,
+  CTD_HP_CN050AE,
+  CTD_HP_CN051AE,
+  CTD_HP_CN052AE,
+  CTD_HP_CN053AE,
+  CTD_HP_CN054AE,
+  CTD_HP_CN055AE,
+  CTD_HP_CN056AE,
+  CTD_HP_CN057AE,
+  CTD_HP_CZ101A,
+  CTD_HP_CZ102A,
+  CTD_HP_F6U12AE,
+  CTD_HP_F6U13AE,
+  CTD_HP_F6U14AE,
+  CTD_HP_F6V16AE,
+  CTD_HP_L0S58AE,
+  CTD_HP_M0H54AE,
+  CTD_HP_M0H55AE,
+  CTD_HP_M0H56AE,
+  CTD_HP_N9K05AE,
+  CTL_HP_C6511,
+  CTL_HP_C7115A_AD,
+  CTL_HP_CB435A,
+  CTL_HP_CB540A,
+  CTL_HP_CB542A,
+  CTL_HP_CB543A,
+  CTL_HP_CE255A,
+  CTL_HP_CE255X,
+  CTL_HP_CE278A,
+  CTL_HP_CE285A,
+  CTL_HP_CE310A,
+  CTL_HP_CE311A,
+  CTL_HP_CE312A,
+  CTL_HP_CE313A,
+  CTL_HP_CE320A,
+  CTL_HP_CE321A,
+  CTL_HP_CE322A,
+  CTL_HP_CE323A,
+  CTL_HP_CE390A,
+  CTL_HP_CE400A,
+  CTL_HP_CE401A,
+  CTL_HP_CE402A,
+  CTL_HP_CE403A,
+  CTL_HP_CE410A,
+  CTL_HP_CE410X,
+  CTL_HP_CE411A,
+  CTL_HP_CE412A,
+  CTL_HP_CE413A,
+  CTL_HP_CE505A,
+  CTL_HP_CE505XD,
+  CTL_HP_CE740A,
+  CTL_HP_CE741A,
+  CTL_HP_CE742A,
+  CTL_HP_CE743A,
+  CTL_HP_CF210A,
+  CTL_HP_CF211A,
+  CTL_HP_CF212A,
+  CTL_HP_CF213A,
+  CTL_HP_CF217,
+  CTL_HP_CF226A,
+  CTL_HP_CF226X,
+  CTL_HP_CF230A,
+  CTL_HP_CF244A,
+  CTL_HP_CF259A,
+  CTL_HP_CF259X,
+  CTL_HP_CF280A,
+  CTL_HP_CF280X,
+  CTL_HP_CF281A,
+  CTL_HP_CF283A,
+  CTL_HP_CF289A,
+  CTL_HP_CF360A,
+  CTL_HP_CF360X,
+  CTL_HP_CF361A,
+  CTL_HP_CF361X,
+  CTL_HP_CF362A,
+  CTL_HP_CF362X,
+  CTL_HP_CF363A,
+  CTL_HP_CF363X,
+  CTL_HP_CF400A,
+  CTL_HP_CF401A,
+  CTL_HP_CF402A,
+  CTL_HP_CF403A,
+  CTL_HP_CF410A,
+  CTL_HP_CF410X,
+  CTL_HP_CF411A,
+  CTL_HP_CF411X,
+  CTL_HP_CF412A,
+  CTL_HP_CF412X,
+  CTL_HP_CF413A,
+  CTL_HP_CF413X,
+  CTL_HP_CF530A,
+  CTL_HP_CF531A,
+  CTL_HP_CF532A,
+  CTL_HP_CF533A,
+  CTL_HP_CF540A,
+  CTL_HP_CF541A,
+  CTL_HP_CF541X,
+  CTL_HP_CF542X,
+  CTL_HP_CF543X,
+  CTL_HP_Q2612A,
+  CTL_HP_Q2624A,
+  CTL_HP_Q5949A,
+  CTL_HP_Q5952AC,
+  CTL_HP_Q5953A,
+  CTL_HP_Q7551A,
+  CTL_HP_Q7551X,
+  CTL_HP_Q7553A,
+  CTL_HP_W1106A,
+  CTL_HP_W1335A,
+  CTL_HP_W1360A,
+  CTL_HP_W1500A,
+  CTL_HP_W2030X,
+  CTL_HP_W2031A,
+  CTL_HP_W2032A,
+  CTL_HP_W2033A,
+  CTL_HP_W2071A,
+  CTL_HP_W2210A,
+  CTL_HP_W2211A,
+  CTL_HP_W2212A,
+  CTL_HP_W2213A,
+  CTL_HP_W2410A,
+  CTL_HP_W2411A,
+  CTL_HP_W2412A,
+  CTL_HP_W2413A,
 };
