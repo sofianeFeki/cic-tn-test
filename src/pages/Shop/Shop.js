b0321a13c5220b121c0eb1f07f8a1955729b4ffb
sofianeFeki <backofficekompar@gmail.com>
2024-07-16 13:04:06 +0100

    Update Shop.js

--- src/pages/Shop/Shop.js
+++ src/pages/Shop/Shop.js
@@ -1,29 +1,92 @@
-import React, { useState } from "react";
-import Breadcrumbs from "../../components/pageProps/Breadcrumbs";
-import Pagination from "../../components/pageProps/shopPage/Pagination";
-import ProductBanner from "../../components/pageProps/shopPage/ProductBanner";
-import ShopSideNav from "../../components/pageProps/shopPage/ShopSideNav";
+import React, { useEffect, useState } from 'react';
+import Breadcrumbs from '../../components/pageProps/Breadcrumbs';
+import Pagination from '../../components/pageProps/shopPage/Pagination';
+import ProductBanner from '../../components/pageProps/shopPage/ProductBanner';
+import ShopSideNav from '../../components/pageProps/shopPage/ShopSideNav';
+import { getProducts } from '../../functions/product';
 
 const Shop = () => {
   const [itemsPerPage, setItemsPerPage] = useState(12);
+  const [sort, setSort] = useState('');
+  const [itemOffset, setItemOffset] = useState(0); // Added itemOffset state
+  const [totalPages, setTotalPages] = useState(1);
+  const [totalProducts, setTotalProducts] = useState(1);
+  const [products, setProducts] = useState([]);
+  const [loading, setLoading] = useState(false);
+
   const itemsPerPageFromBanner = (itemsPerPage) => {
     setItemsPerPage(itemsPerPage);
   };
 
+  const sortFromBanner = (sort) => {
+    setSort(sort);
+  };
+
+  const gettheProducts = (page, sort, itemsPerPage) => {
+    setLoading(true);
+    getProducts(page, sort, itemsPerPage)
+      .then((res) => {
+        const productData = res.data.products;
+        const baseUrl = 'https://cic-server-ygl9.onrender.com';
+
+        const formatUrl = (path) => `${baseUrl}${path.replace(/\\/g, '/')}`;
+
+        productData.forEach((product) => {
+          if (product.Image) {
+            product.Image = formatUrl(product.Image);
+          }
+          if (product.video) {
+            product.video = formatUrl(product.video);
+          }
+          if (product.pdf) {
+            product.pdf = formatUrl(product.pdf);
+          }
+        });
+
+        setProducts(productData);
+        setTotalPages(res.data.totalPages);
+        setTotalProducts(res.data.totalProducts);
+        setLoading(false);
+        console.log(productData, 'Product data loaded');
+      })
+      .catch((error) => {
+        console.error('Failed to fetch products', error);
+        setLoading(false);
+      });
+  };
+
+  useEffect(() => {
+    const page = Math.floor(itemOffset / itemsPerPage) + 1;
+    gettheProducts(page, sort, itemsPerPage);
+  }, [itemOffset, sort, itemsPerPage]);
+
+  const handlePageClick = (event) => {
+    const newOffset = event.selected * itemsPerPage;
+    setItemOffset(newOffset);
+  };
+
   return (
-    <div className='max-w-container mx-auto px-4'>
-      <Breadcrumbs title='Produits' />
-      {/* ================= Products Start here =================== */}
-      <div className='w-full h-full flex pb-20 gap-10'>
-        <div className='w-[20%] lgl:w-[25%] hidden mdl:inline-flex h-full'>
+    <div className="max-w-container mx-auto px-4">
+      <Breadcrumbs title="Produits" />
+      <div className="w-full h-full flex pb-20 gap-10">
+        <div className="w-[20%] lgl:w-[25%] hidden mdl:inline-flex h-full">
           <ShopSideNav />
         </div>
-        <div className='w-full mdl:w-[80%] lgl:w-[75%] h-full flex flex-col gap-10'>
-          <ProductBanner itemsPerPageFromBanner={itemsPerPageFromBanner} />
-          <Pagination itemsPerPage={itemsPerPage} />
+        <div className="w-full mdl:w-[80%] lgl:w-[75%] h-full flex flex-col gap-10">
+          <ProductBanner
+            itemsPerPageFromBanner={itemsPerPageFromBanner}
+            sortFromBanner={sortFromBanner}
+          />
+          <Pagination
+            itemsPerPage={itemsPerPage}
+            products={products}
+            totalPages={totalPages}
+            totalProducts={totalProducts}
+            handlePageClick={handlePageClick}
+            itemOffset={itemOffset}
+          />
         </div>
       </div>
-      {/* ================= Products End here ===================== */}
     </div>
   );
 };
