76d19a8ec4042eee07b5bfef6462dd31ca11f5cb
sofianeFeki <backofficekompar@gmail.com>
2024-07-24 11:51:53 +0100

    Update Category.js

--- src/components/pageProps/shopPage/shopBy/Category.js
+++ src/components/pageProps/shopPage/shopBy/Category.js
@@ -1,16 +1,18 @@
 import React, { useState } from 'react';
 import { useDispatch, useSelector } from 'react-redux';
-import { toggleCategory } from '../../../../redux/orebiSlice';
-import { ImPlus } from 'react-icons/im';
+import { toggleFilter } from '../../../../redux/orebiSlice';
 import NavTitle from './NavTitle';
 
 const Category = () => {
-  const checkedCategorys = useSelector((state) => state.orebi.checkedCategorys);
+  const checkedCategories = useSelector(
+    (state) => state.orebi.filters.category
+  );
   const dispatch = useDispatch();
 
-  const category = [
+  const categories = [
     { _id: 9006, title: 'Imprimante' },
-    { _id: 9007, title: 'Encre' },
+    { _id: 9012, title: 'Photocopieur' },
+    { _id: 9007, title: 'Bouteille D’encre' },
     { _id: 9008, title: 'Ruban' },
     { _id: 9009, title: 'Bac' },
     { _id: 9010, title: 'Toner' },
@@ -18,14 +20,16 @@ const Category = () => {
   ];
 
   const handleToggleCategory = (category) => {
-    dispatch(toggleCategory(category));
+    dispatch(
+      toggleFilter({ filterType: 'category', filterValue: category.title })
+    );
   };
 
   return (
     <div className="w-full p-4 bg-[#F5F5F3] rounded-lg drop-shadow-xl">
       <NavTitle title="Catégories" icons={true} />
       <ul className="flex flex-col gap-3 text-gray-700">
-        {category.map((item) => (
+        {categories.map((item) => (
           <li
             key={item._id}
             className="flex items-center justify-between border-b border-gray-200 py-2 hover:text-primeColor transition-colors duration-300"
@@ -38,7 +42,7 @@ const Category = () => {
                 className="mr-2 accent-primeColor cursor-pointer"
                 type="checkbox"
                 id={item._id}
-                checked={checkedCategorys.some((b) => b._id === item._id)}
+                checked={checkedCategories.includes(item.title)}
                 onChange={() => handleToggleCategory(item)}
               />
               {item.title}
