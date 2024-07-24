2cb9288e0811e1951f2661bbb90eacf6d5f7b187
sofianeFeki <backofficekompar@gmail.com>
2024-07-24 11:57:59 +0100

    Update ProductCreate.js

--- src/pages/ProductDetails/ProductCreate.js
+++ src/pages/ProductDetails/ProductCreate.js
@@ -1,65 +1,70 @@
-import React, { useState } from "react";
-import ProductForm from "../../components/forms/productForm";
-import { productCreate } from "../../functions/product";
-import { toast } from "react-toastify";
+import React, { useState } from 'react';
+import ProductForm from '../../components/forms/productForm';
+import { productCreate } from '../../functions/product';
+import { toast } from 'react-toastify';
 
 const initialState = {
-  Title: "",
-  Description: "",
+  Title: '',
+  Description: '',
   Price: 0,
-  Image: "",
+  Image: '',
   Quantity: 0,
-  color: "",
-  Brand: "",
-  Category: "",
-  subCategory: "",
-  ficheTech: [{ label: "", value: "" }],
-  pdf: "",
-  video: "",
+  sold:0,
+  color: '',
+  Brand: '',
+  Category: '',
+  subCategory: '',
+  ficheTech: [{ label: '', value: '' }],
+  pdf: '',
+  video: '',
 };
 
 const ProductCreate = () => {
   const [product, setProduct] = useState(initialState);
   const [startEdit, setStartEdit] = useState(true);
-  const [imagePreview, setImagePreview] = useState(""); // State for image preview
-  const [videoPreview, setVideoPreview] = useState(""); // State for video preview
-  const [pdfPreview, setPdfPreview] = useState(""); // State for PDF preview
+  const [imagePreview, setImagePreview] = useState(''); // State for image preview
+  const [videoPreview, setVideoPreview] = useState(''); // State for video preview
+  const [pdfPreview, setPdfPreview] = useState(''); // State for PDF preview
+  const [loading, setLoading] = useState(false);
 
   const handleSubmit = async (e) => {
     e.preventDefault();
+    setLoading(true);
+
     const formData = new FormData();
 
     // Append all fields except 'ficheTech', 'pdf', 'video', and 'Image'
     for (const key in product) {
       if (
-        key !== "ficheTech" &&
-        key !== "pdf" &&
-        key !== "video" &&
-        key !== "Image"
+        key !== 'ficheTech' &&
+        key !== 'pdf' &&
+        key !== 'video' &&
+        key !== 'Image'
       ) {
         formData.append(key, product[key]);
       }
     }
 
-    formData.append("ficheTech", JSON.stringify(product.ficheTech));
+    formData.append('ficheTech', JSON.stringify(product.ficheTech));
 
     // Append the image file if it exists and 'Image' field isn't already appended
-    if (product.imageFile && !formData.has("imageFile")) {
-      formData.append("imageFile", product.imageFile);
+    if (product.imageFile && !formData.has('imageFile')) {
+      formData.append('imageFile', product.imageFile);
     }
 
     // Append 'pdf' and 'video' if they exist
-    if (product.pdf) formData.append("pdf", product.pdf);
-    if (product.video) formData.append("video", product.video);
+    if (product.pdf) formData.append('pdf', product.pdf);
+    if (product.video) formData.append('video', product.video);
 
     try {
       const res = await productCreate(formData);
-      console.log("Response:", res);
+      console.log('Response:', res);
       toast.success(`"${res.data.Title}" is created`);
       //window.location.reload();
     } catch (err) {
       toast.error(err.response.data.err);
     }
+    setLoading(false);
   };
 
   return (
@@ -75,6 +80,7 @@ const ProductCreate = () => {
         setVideoPreview={setVideoPreview} // Pass setVideoPreview to the form
         pdfPreview={pdfPreview} // Pass pdfPreview to the form
         setPdfPreview={setPdfPreview} // Pass setPdfPreview to the form
+        loading={loading}
       />
     </>
   );
