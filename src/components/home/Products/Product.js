4349cbf2afb161b7622acb5da72a234dc4bfa0cb
sofianeFeki <backofficekompar@gmail.com>
2024-07-24 11:50:17 +0100

    Update Product.js

--- src/components/home/Products/Product.js
+++ src/components/home/Products/Product.js
@@ -34,25 +34,36 @@ const Product = (props) => {
       : 'w-full flex gap-4 p-4 border-b bg-white shadow-lg rounded-lg cursor-pointer';
   const imageClass =
     viewMode === 'grid'
-      ? 'w-full h-48 relative overflow-hidden'
-      : 'w-1/3 h-48 overflow-hidden rounded-lg';
+      ? 'w-full h-full relative overflow-hidden'
+      : 'w-1/3 h-full overflow-hidden rounded-lg';
   const detailsClass =
     viewMode === 'grid'
-      ? 'w-full bg-white h-32 flex flex-col justify-between p-4'
+      ? 'w-full bg-white h-38 flex flex-col justify-between p-4'
       : 'w-2/3 flex flex-col justify-center';
 
   return (
     <div className={containerClass}>
       <div className={imageClass}>
-        <div onClick={handleProductDetails} className="w-full h-full">
-          <Image className="w-full h-full object-cover" imgSrc={props.img} />
-        </div>
-        {viewMode === 'grid' && (
+        {props.loading ? (
+          <div class="shadow rounded-md p-4 max-w-sm w-full mx-auto">
+            <div class="animate-pulse flex space-x-4">
+              <div class="flex-1 space-y-6 py-1">
+                <div class="h-48  bg-slate-100 rounded"></div>
+              </div>
+            </div>
+          </div>
+        ) : (
+          <div onClick={handleProductDetails} className="w-full h-full">
+            <Image className="w-full h-full object-cover" imgSrc={props.img} />
+          </div>
+        )}
+
+        {/* {viewMode === 'grid' && (
           <div className="absolute top-6 left-8">
             {props.badge && <Badge text="New" />}
           </div>
-        )}
-        {viewMode === 'grid' && (
+        )} */}
+        {viewMode === 'grid' && !props.loading && (
           <div className="w-full h-32 absolute bg-white -bottom-[130px] group-hover:bottom-0 duration-700">
             <ul className="w-full h-full flex flex-col items-end justify-center gap-2 font-titleFont px-2 border-l border-r">
               <li className="text-[#767676] hover:text-primeColor text-sm font-normal border-b-[1px] border-b-gray-200 hover:border-b-primeColor flex items-center justify-end gap-2 hover:cursor-pointer pb-1 duration-300 w-full">
@@ -103,23 +114,38 @@ const Product = (props) => {
           </div>
         )}
       </div>
-      <div className={detailsClass}>
-        <div className="max-w-80 py-6 flex flex-col gap-1 px-1">
-          <div className="flex items-center justify-between font-titleFont">
-            <h2 className="text-lg text-primeColor font-bold">
-              {props.productName}
-            </h2>
-          </div>
-          {viewMode === 'list' && (
-            <div>
-              <p className="text-[#767676] text-[16px]">{props.des}</p>
+      {props.loading ? (
+        <div class=" p-4  w-full mx-auto">
+          <div class="animate-pulse flex space-x-4">
+            <div class="flex-1 space-y-6 py-1">
+              <div class="h-4 bg-slate-100 rounded"></div>
+              <div class="space-y-3">
+                <div class="grid grid-cols-3 gap-4">
+                  <div class="h-4 bg-slate-100 rounded col-span-1"></div>
+                </div>
+              </div>
             </div>
-          )}
-          <div>
-            <p className="text-[#767676] text-[14px]">{props.color}</p>
           </div>
         </div>
-      </div>
+      ) : (
+        <div className={detailsClass}>
+          <div className="max-w-80 py-6 flex flex-col gap-1 px-1">
+            <div className="flex items-center justify-between font-titleFont">
+              <h2 className="text-lg text-primeColor font-bold">
+                {props.productName}
+              </h2>
+            </div>
+            {viewMode === 'list' && (
+              <div>
+                <p className="text-[#767676] text-[16px]">{props.des}</p>
+              </div>
+            )}
+            <div>
+              <p className="text-[#767676] text-[14px]">{props.color}</p>
+            </div>
+          </div>
+        </div>
+      )}
     </div>
   );
 };
