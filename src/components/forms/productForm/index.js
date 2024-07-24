fbd42bfc468e88ac580926fa01accbd039046a62
sofianeFeki <backofficekompar@gmail.com>
2024-07-24 11:45:03 +0100

    Update index.js

--- src/components/forms/productForm/index.js
+++ src/components/forms/productForm/index.js
@@ -1,6 +1,5 @@
-import React, { useEffect, useState } from 'react';
-import { Link, useLocation, useNavigate, useParams } from 'react-router-dom';
-import { FaDownload } from 'react-icons/fa';
+import React, { useState } from 'react';
+import { useNavigate, useParams } from 'react-router-dom';
 import { useSelector } from 'react-redux';
 import { Field, Label, Select } from '@headlessui/react';
 import {
@@ -16,18 +15,33 @@ import {
 } from '@heroicons/react/24/outline';
 import Breadcrumbs from '../../pageProps/Breadcrumbs';
 import { noImage } from '../../../assets/images';
-import { productCreate } from '../../../functions/product';
+import { removeProduct } from '../../../functions/product';
+import { toast } from 'react-toastify';
 
-const Colors = ['Black', 'Blanc', 'Yellow', 'Cyan', 'Magenta'];
-const Brands = ['Pantum', 'HP', 'Epson', 'Ricoh'];
-const Categories = ['Imprimante', 'photocopieur', 'Consommable'];
+const Colors = ['Black', 'Blanc', 'Yellow', 'Cyan', 'Magenta', 'Multicouleur'];
+const Brands = [
+  'Pantum',
+  'HP',
+  'Epson',
+  'Ricoh',
+  'Canon',
+  'Kyocera',
+  'Oki',
+  'Tally',
+  'Epson',
+  'Samsung',
+  'Lexmark',
+  'xerox',
+];
+const Categories = ['Imprimante', 'Photocopieur', 'Consommable'];
 const subCategory = [
-  'Imprimante',
-  'Tonner',
+  'Toner',
   'Bac de dechet',
   'Cartouche',
   'Ruban',
-  'Encre',
+  'Bouteille D’encre',
+  'Tête d’impression',
+  'Tambour',
 ];
 
 const ProductForm = ({
@@ -36,7 +50,7 @@ const ProductForm = ({
   handleSubmit,
   startEdit,
   setStartEdit,
-
+  loading,
   videoPreview,
   setVideoPreview,
   pdfPreview,
@@ -48,7 +62,6 @@ const ProductForm = ({
   const user = useSelector((state) => state.orebi.userInfo);
   const [activeTab, setActiveTab] = useState('');
   const navigate = useNavigate();
-  const { slug } = useParams();
 
   const handleEditClick = () => {
     navigate(`/admin/product-update/${product.slug}`, { state: { product } });
@@ -152,6 +165,22 @@ const ProductForm = ({
     if (field === 'video') setVideoPreview('');
     if (field === 'pdf') setPdfPreview('');
     console.log(product);
+  };
+
+  const handleRemove = () => {
+    // let answer = window.confirm('Delete');
+    if (window.confirm('Delete')) {
+      // console.log('send delete request', slug);
+      removeProduct(product.slug)
+        .then((res) => {
+          toast.error(`${res.data.Title} is deleted`);
+          navigate('/shop');
+        })
+        .catch((err) => {
+          if (err.response.status === 400) toast.error(err.response.data);
+          console.log(err);
+        });
+    }
   };
 
   const tabs = [
@@ -437,6 +466,21 @@ const ProductForm = ({
                       <h2 className="text-4xl font-semibold break-words">
                         {product.Title}
                       </h2>
+                      {/* <div class="border border-blue-300 shadow rounded-md p-4 max-w-sm w-full mx-auto">
+                        <div class="animate-pulse flex space-x-4">
+                          <div class="rounded-full bg-slate-200 h-10 w-10"></div>
+                          <div class="flex-1 space-y-6 py-1">
+                            <div class="h-2 bg-slate-200 rounded"></div>
+                            <div class="space-y-3">
+                              <div class="grid grid-cols-3 gap-4">
+                                <div class="h-2 bg-slate-200 rounded col-span-2"></div>
+                                <div class="h-2 bg-slate-200 rounded col-span-1"></div>
+                              </div>
+                              <div class="h-2 bg-slate-200 rounded"></div>
+                            </div>
+                          </div>
+                        </div>
+                      </div> */}
                     </div>
                   )}
                 </div>
@@ -455,13 +499,34 @@ const ProductForm = ({
                           <span className="font-bold">Annuler</span>
                         </button>
                         <button
-                          className="flex items-center p-2 bg-blue-500 text-white rounded cursor-pointer shadow-md hover:bg-blue-600 transition duration-300"
+                          className="flex items-center p-2 bg-blue-500 text-white rounded cursor-pointer shadow-md hover:bg-blue-600 transition duration-300 disabled:bg-gray-400 disabled:cursor-not-allowed"
                           onClick={handleSubmit}
+                          disabled={loading} // Change to true to see the disabled state or use a dynamic condition
                         >
-                          <DocumentCheckIcon
-                            className="block h-6 w-6 mr-1"
-                            aria-hidden="true"
-                          />
+                          {loading ? (
+                            <svg
+                              aria-hidden="true"
+                              role="status"
+                              className="inline mr-2 w-4 h-4 text-gray-200 animate-spin dark:text-gray-600"
+                              viewBox="0 0 100 101"
+                              fill="none"
+                              xmlns="http://www.w3.org/2000/svg"
+                            >
+                              <path
+                                d="M100 50.5908C100 78.2051 77.6142 100.591 50 100.591C22.3858 100.591 0 78.2051 0 50.5908C0 22.9766 22.3858 0.59082 50 0.59082C77.6142 0.59082 100 22.9766 100 50.5908ZM9.08144 50.5908C9.08144 73.1895 27.4013 91.5094 50 91.5094C72.5987 91.5094 90.9186 73.1895 90.9186 50.5908C90.9186 27.9921 72.5987 9.67226 50 9.67226C27.4013 9.67226 9.08144 27.9921 9.08144 50.5908Z"
+                                fill="currentColor"
+                              ></path>
+                              <path
+                                d="M93.9676 39.0409C96.393 38.4038 97.8624 35.9116 97.0079 33.5539C95.2932 28.8227 92.871 24.3692 89.8167 20.348C85.8452 15.1192 80.8826 10.7238 75.2124 7.41289C69.5422 4.10194 63.2754 1.94025 56.7698 1.05124C51.7666 0.367541 46.6976 0.446843 41.7345 1.27873C39.2613 1.69328 37.813 4.19778 38.4501 6.62326C39.0873 9.04874 41.5694 10.4717 44.0505 10.1071C47.8511 9.54855 51.7191 9.52689 55.5402 10.0491C60.8642 10.7766 65.9928 12.5457 70.6331 15.2552C75.2735 17.9648 79.3347 21.5619 82.5849 25.841C84.9175 28.9121 86.7997 32.2913 88.1811 35.8758C89.083 38.2158 91.5421 39.6781 93.9676 39.0409Z"
+                                fill="#1C64F2"
+                              ></path>
+                            </svg>
+                          ) : (
+                            <DocumentCheckIcon
+                              className="block h-6 w-6 mr-1"
+                              aria-hidden="true"
+                            />
+                          )}
                           <span className="font-bold">Sauvegarder</span>
                         </button>
                       </>
@@ -469,7 +534,8 @@ const ProductForm = ({
                       <>
                         <button
                           onClick={handleEditClick}
-                          className="flex items-center p-2 bg-white text-black rounded cursor-pointer shadow-md hover:bg-gray-300 transition duration-300"
+                          className="flex items-center p-2 bg-white text-black rounded cursor-pointer shadow-md hover:bg-gray-300 transition duration-300 disabled:bg-gray-300 disabled:cursor-not-allowed"
+                          disabled={!product.slug}
                         >
                           <AdjustmentsVerticalIcon
                             className="block h-6 w-6 mr-1"
@@ -479,7 +545,7 @@ const ProductForm = ({
                         </button>
                         <button
                           className="flex items-center p-2 bg-blue-500 text-white rounded cursor-pointer shadow-md hover:bg-blue-600 transition duration-300"
-                          //onClick={handleDelete} // Assuming you have a delete handler function
+                          onClick={handleRemove} // Assuming you have a delete handler function
                         >
                           <TrashIcon
                             className="block h-6 w-6 mr-1"
@@ -527,6 +593,17 @@ const ProductForm = ({
                         type="number"
                         className="block w-[50%] rounded-lg  bg-white py-1.5 px-3 text-sm/4 text-black focus:outline-none focus:ring focus:border-blue-300 transition duration-150 shadow-md"
                         value={product.Quantity}
+                        onChange={handleChangeProduct}
+                      />
+                    </Field>
+                    <Field>
+                      <Label className="text-sm/6 font-medium">Sold</Label>
+                      <input
+                        id="sold"
+                        name="sold"
+                        type="number"
+                        className="block w-[50%] rounded-lg  bg-white py-1.5 px-3 text-sm/4 text-black focus:outline-none focus:ring focus:border-blue-300 transition duration-150 shadow-md"
+                        value={product.sold}
                         onChange={handleChangeProduct}
                       />
                     </Field>
