89945bdb4b0a185412ca6532ab2cd8a45d6dcec0
sofianeFeki <backofficekompar@gmail.com>
2024-08-07 11:39:24 +0100

    Update formOptions.js

--- src/components/forms/productForm/formOptions.js
+++ src/components/forms/productForm/formOptions.js
@@ -1,1 +1,38 @@
+export const Colors = [
+  'Noir',
+  'Blanc',
+  'Jaune',
+  'Bleu',
+  'Magenta',
+  'Multicouleur',
+];
+export const Brands = [
+  'Pantum',
+  'HP',
+  'Epson',
+  'Ricoh',
+  'Canon',
+  'Brother',
+  'Toshiba',
+  'Printronix',
+  'Fujitsu',
+  'Compuprint',
+  'Kyocera',
+  'Oki',
+  'Tally',
+  'Samsung',
+  'Lexmark',
+  'xerox',
+  'konika minolta',
+];
+export const Categories = ['Imprimante', 'Photocopieur', 'Consommable'];
+export const subCategory = [
+  'Toner',
+  'Cartouche D’encre',
+  'Bouteille D’encre',
+  'Bac de dechet',
+  'Tête d’impression',
+  'Ruban',
+  'Tambour',
+];
 
