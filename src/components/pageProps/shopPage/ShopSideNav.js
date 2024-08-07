0a416fc5bca59d5591500938f0d831c06a304ad6
sofianeFeki <backofficekompar@gmail.com>
2024-08-07 11:38:40 +0100

    Update ShopSideNav.js

--- src/components/pageProps/shopPage/ShopSideNav.js
+++ src/components/pageProps/shopPage/ShopSideNav.js
@@ -1,13 +1,15 @@
-import React from "react";
-import Brand from "./shopBy/Brand";
-import Category from "./shopBy/Category";
-import Color from "./shopBy/Color";
+import React from 'react';
+import Brand from './shopBy/Brand';
+import Category from './shopBy/Category';
+import Color from './shopBy/Color';
+import SubCategory from './shopBy/subCtegory';
 //import Price from "./shopBy/Price";
 
 const ShopSideNav = () => {
   return (
     <div className="w-full flex flex-col gap-6">
       <Category icons={false} />
+      <SubCategory />
       <Brand />
       <Color />
       {/* <Price /> */}
