0dcec48d081b28499f13d47701da08ab6a7f35b6
sofianeFeki <backofficekompar@gmail.com>
2024-08-07 11:40:35 +0100

    Update Category.js

--- src/components/pageProps/shopPage/shopBy/Category.js
+++ src/components/pageProps/shopPage/shopBy/Category.js
@@ -2,6 +2,7 @@ import React, { useState } from 'react';
 import { useDispatch, useSelector } from 'react-redux';
 import { toggleFilter } from '../../../../redux/orebiSlice';
 import NavTitle from './NavTitle';
+import { Categories } from '../../../forms/productForm/formOptions';
 
 const Category = () => {
   const checkedCategories = useSelector(
@@ -9,43 +10,28 @@ const Category = () => {
   );
   const dispatch = useDispatch();
 
-  const categories = [
-    { _id: 9006, title: 'Imprimante' },
-    { _id: 9012, title: 'Photocopieur' },
-    { _id: 9007, title: 'Bouteille D’encre' },
-    { _id: 9008, title: 'Ruban' },
-    { _id: 9009, title: 'Bac' },
-    { _id: 9010, title: 'Toner' },
-    { _id: 9011, title: 'Cartouche' },
-  ];
-
   const handleToggleCategory = (category) => {
-    dispatch(
-      toggleFilter({ filterType: 'category', filterValue: category.title })
-    );
+    dispatch(toggleFilter({ filterType: 'category', filterValue: category }));
   };
 
   return (
     <div className="w-full p-4 bg-[#F5F5F3] rounded-lg drop-shadow-xl">
       <NavTitle title="Catégories" icons={true} />
       <ul className="flex flex-col gap-3 text-gray-700">
-        {categories.map((item) => (
+        {Categories.map((item) => (
           <li
-            key={item._id}
+            key={item}
             className="flex items-center justify-between border-b border-gray-200 py-2 hover:text-primeColor transition-colors duration-300"
           >
-            <label
-              htmlFor={item._id}
-              className="flex items-center cursor-pointer"
-            >
+            <label htmlFor={item} className="flex items-center cursor-pointer">
               <input
                 className="mr-2 accent-primeColor cursor-pointer"
                 type="checkbox"
-                id={item._id}
-                checked={checkedCategories.includes(item.title)}
+                id={item}
+                checked={checkedCategories.includes(item)}
                 onChange={() => handleToggleCategory(item)}
               />
-              {item.title}
+              {item}
             </label>
           </li>
         ))}
