a928716e15aee1557e97d70740e19e7de11036e5
sofianeFeki <backofficekompar@gmail.com>
2024-08-21 21:35:57 +0100

    Update Home.js

--- src/pages/Home/Home.js
+++ src/pages/Home/Home.js
@@ -1,22 +1,43 @@
-import React from 'react';
+import React, { useEffect } from 'react';
 import Banner from '../../components/Banner/Banner';
 import BannerBottom from '../../components/Banner/BannerBottom';
 import BestSellers from '../../components/home/BestSellers/BestSellers';
 import NewArrivals from '../../components/home/NewArrivals/NewArrivals';
 import Sale from '../../components/home/Sale/Sale';
-import SpecialOffers from '../../components/home/SpecialOffers/SpecialOffers';
 import YearProduct from '../../components/home/YearProduct/YearProduct';
+import { useDispatch } from 'react-redux';
+import { setFilter, setViewMode } from '../../redux/orebiSlice';
+import { homeBg } from '../../assets/images';
 
 const Home = () => {
+  const dispatch = useDispatch();
+
+  useEffect(() => {
+    dispatch(setViewMode('grid'));
+    dispatch(setFilter());
+  }, [dispatch]);
+
   return (
-    <div className="w-full mx-auto">
-      <Banner />
-      <BannerBottom />
-      <div className="max-w-container mt-6 mx-auto px-4">
-        <NewArrivals />
-        <Sale />
-        <BestSellers />
-        <YearProduct />
+    <div className="w-full mx-auto  relative">
+      <div
+        className="fixed w-full h-full top-0 left-0 z-0"
+        style={{
+          backgroundImage: `url(${homeBg})`, // Correct way to set the background image
+          backgroundAttachment: 'fixed',
+          backgroundSize: 'cover',
+          backgroundPosition: 'center',
+          opacity: 0.5, // Adjust the opacity value
+        }}
+      ></div>
+      <div className="relative z-10 min-h-screen">
+        <Banner />
+        <BannerBottom />
+        <div className="max-w-container mt-6 mx-auto px-4">
+          <NewArrivals />
+          <Sale />
+          <BestSellers />
+          <YearProduct />
+        </div>
       </div>
     </div>
   );
