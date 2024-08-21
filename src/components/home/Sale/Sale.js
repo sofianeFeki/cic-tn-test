3d52e684a298292bffa3d0c797a845d48a32bbd3
sofianeFeki <backofficekompar@gmail.com>
2024-08-21 21:37:11 +0100

    Update Sale.js

--- src/components/home/Sale/Sale.js
+++ src/components/home/Sale/Sale.js
@@ -1,53 +1,102 @@
-import React from "react";
-import { Link } from "react-router-dom";
+import React from 'react';
+import { Link } from 'react-router-dom';
 import {
   saleImgOne,
   saleImgTwo,
   saleImgThree,
-} from "../../../assets/images/index";
-import Image from "../../designLayouts/Image";
-import ShopNow from "../../designLayouts/buttons/ShopNow";
+  consommable,
+  ricoh1,
+  ricoh2,
+  ricoh3,
+  pantum1,
+  pantum2,
+  pantum3,
+} from '../../../assets/images/index';
 
 const Sale = () => {
   return (
-    <div className="py-20 flex flex-col md:flex-row items-center justify-between gap-4 lg:gap-10">
-      <div className="bg-[#f3f3f3] w-full md:w-2/3 lg:w-1/2 h-full flex flex-col justify-center items-center text-black  drop-shadow-xl overflow-x-hidden group cursor-pointer">
-        <div className="aspect-w-4 aspect-h-3 w-full mb-4">
-          <Image className="h-full w-full object-cover" imgSrc={saleImgOne} />
-        </div>
-        <div className="text-left h-140 md:h-200 lg:h-260 w-full mx-4 ">
-          <div className="mx-8">
-            <h2 className="text-4xl md:text-5xl lg:text-5xl font-bold mb-6">
-              Imprimante sales
-            </h2>
-            <p className="text-lg md:text-xl lg:text-2xl mb-6">
-              Up to{" "}
-              <span className="text-4xl md:text-5xl lg:text-5xl font-bold">
-                30%
-              </span>{" "}
-              sales for all impriamnte{" "}
-            </p>
-            <div className=" mb-8">
-              <ShopNow />
-            </div>
+    <div className="py-20 flex flex-col lg:flex-row items-center justify-between gap-10">
+      {/* Other Sections */}
+      <div className="flex flex-col gap-10 w-full lg:w-2/3">
+        {/* Imprimantes Section */}
+        <div className="bg-white p-6 drop-shadow-lg">
+          <p className="text-2xl font-semibold mb-4">
+            Découvrir nos imprimantes
+          </p>
+          <div className="flex gap-4">
+            <img
+              src={pantum1}
+              alt="Imprimante 1"
+              className="w-1/3 max-h-[156px] h-full object-cover cursor-pointer transform transition duration-300 hover:scale-105"
+            />
+            <img
+              src={pantum2}
+              alt="Imprimante 2"
+              className="w-1/3 max-h-[170px] h-full object-cover cursor-pointer transform transition duration-300 hover:scale-105"
+            />
+            <img
+              src={pantum3}
+              alt="Imprimante 3"
+              className="w-1/3 max-h-[170px] h-full object-cover cursor-pointer transform transition duration-300 hover:scale-105"
+            />
           </div>
+          <Link
+            to="category/Imprimante"
+            className="text-blue-500 hover:underline block mt-4"
+          >
+            Découvrir plus
+          </Link>
+        </div>
+
+        {/* Photocopieur Section */}
+        <div className="bg-white p-6 drop-shadow-lg">
+          <p className="text-2xl font-semibold mb-4">
+            Découvrir nos Photocopieur
+          </p>
+          <div className="flex gap-4">
+            <img
+              src={ricoh1}
+              alt="Photocopieur 1"
+              className="w-1/3 max-h-[156px] h-full object-cover cursor-pointer transform transition duration-300 hover:scale-105"
+            />
+            <img
+              src={ricoh2}
+              alt="Photocopieur 2"
+              className="w-1/3 max-h-[156px] h-full object-cover cursor-pointer transform transition duration-300 hover:scale-105"
+            />
+            <img
+              src={ricoh3}
+              alt="Photocopieur 3"
+              className="w-1/3 max-h-[156px] h-full object-cover cursor-pointer transform transition duration-300 hover:scale-105"
+            />
+          </div>
+          <Link
+            to="category/Photocopieur"
+            className="text-blue-500 hover:underline block mt-4"
+          >
+            Découvrir plus
+          </Link>
         </div>
       </div>
 
-      <div className="w-full md:w-2/3 lg:w-1/2 h-auto flex flex-col gap-4 lg:gap-10">
-        <div className="h-1/2 w-full  drop-shadow-xl overflow-x-hidden group cursor-pointer">
-          <Link to="/shop">
-            <Image className="h-full w-full object-cover" imgSrc={saleImgTwo} />
-          </Link>
+      {/* Consommable Section */}
+      <div className="bg-white w-full lg:w-1/3 gap-4 flex flex-col px-8 text-left text-black drop-shadow-lg cursor-pointer">
+        <p className="text-2xl font-semibold mt-3">
+          Découvrir nos consommables
+        </p>
+        <div className="flex-grow flex items-center justify-center">
+          <img
+            src={consommable}
+            alt="Consommable"
+            className="object-cover max-h-[520px] w-[560px] shadow-md transform transition duration-300 hover:scale-105"
+          />
         </div>
-        <div className="h-1/2 w-full  drop-shadow-xl overflow-x-hidden group cursor-pointer">
-          <Link to="/shop">
-            <Image
-              className="h-full w-full object-cover"
-              imgSrc={saleImgThree}
-            />
-          </Link>
-        </div>
+        <Link
+          to="category/Consommable"
+          className="text-blue-500 hover:underline block mb-2"
+        >
+          Découvrir plus
+        </Link>
       </div>
     </div>
   );
