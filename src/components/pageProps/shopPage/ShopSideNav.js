3d945cfdf8ebd8d12e703e658303f6e0bd522404
sofianeFeki <backofficekompar@gmail.com>
2024-08-23 15:35:04 +0100

    Update ShopSideNav.js

--- src/components/pageProps/shopPage/ShopSideNav.js
+++ src/components/pageProps/shopPage/ShopSideNav.js
@@ -3,13 +3,18 @@ import Brand from './shopBy/Brand';
 import Category from './shopBy/Category';
 import Color from './shopBy/Color';
 import SubCategory from './shopBy/subCtegory';
+import { useSelector } from 'react-redux';
 //import Price from "./shopBy/Price";
 
 const ShopSideNav = () => {
+  const checkedCategories = useSelector(
+    (state) => state.orebi.filters.category
+  );
+
   return (
     <div className="w-full flex flex-col gap-6">
       <Category icons={false} />
-      <SubCategory />
+      {checkedCategories.includes('Consommable') && <SubCategory />}
       <Brand />
       <Color />
       {/* <Price /> */}
