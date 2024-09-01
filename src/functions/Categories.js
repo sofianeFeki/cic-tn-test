310a2bdee8296cac29ff1e7f228e3415a37f1945
sofianeFeki <backofficekompar@gmail.com>
2024-09-01 22:09:33 +0100

    Update Categories.js

--- src/functions/Categories.js
+++ src/functions/Categories.js
@@ -1,19 +1,19 @@
 import axios from 'axios';
 
 export const getCategories = async () =>
-  await axios.get('http://localhost:8000/api/categories');
+  await axios.get('https://cic-server-ygl9.onrender.com/api/categories');
 
 export const getCategory = async (slug) =>
-  await axios.get(`http://localhost:8000/api/category/${slug}`);
+  await axios.get(`https://cic-server-ygl9.onrender.com/api/category/${slug}`);
 
 export const removeCategory = async (slug) =>
-  await axios.delete(`http://localhost:8000/api/category/${slug}`, {});
+  await axios.delete(`https://cic-server-ygl9.onrender.com/api/category/${slug}`, {});
 
 export const updateCategory = async (slug, category) =>
-  await axios.put(`http://localhost:8000/api/category/${slug}`, category, {});
+  await axios.put(`https://cic-server-ygl9.onrender.com/api/category/${slug}`, category, {});
 
 export const createCategory = async (category) =>
-  await axios.post(`http://localhost:8000/api/category`, { category });
+  await axios.post(`https://cic-server-ygl9.onrender.com/api/category`, { category });
 
 export const getCategorySubs = async (_id) =>
-  await axios.get(`http://localhost:8000/api/category/subs/${_id}`);
+  await axios.get(`https://cic-server-ygl9.onrender.com/api/category/subs/${_id}`);
