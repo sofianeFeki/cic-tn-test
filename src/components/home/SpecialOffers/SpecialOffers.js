8297407be503450b0b4995b55777cf753183dc01
sofianeFeki <backofficekompar@gmail.com>
2024-09-01 22:17:01 +0100

    Update SpecialOffers.js

--- src/components/home/SpecialOffers/SpecialOffers.js
+++ src/components/home/SpecialOffers/SpecialOffers.js
@@ -59,7 +59,7 @@ const CategoryPage = () => {
       );
       const productData = res.data.products;
 
-      const baseUrl = 'http://localhost:8000';
+      const baseUrl = 'https://cic-server-ygl9.onrender.com';
       const formatUrl = (path) => `${baseUrl}${path.replace(/\\/g, '/')}`;
 
       const formattedProducts = await Promise.all(
