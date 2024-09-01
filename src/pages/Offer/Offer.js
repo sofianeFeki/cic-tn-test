cbf95e8fa6edf87b34b6359b2b99cd1dd24ae4d4
sofianeFeki <backofficekompar@gmail.com>
2024-09-01 21:56:51 +0100

    Update Offer.js

--- src/pages/Offer/Offer.js
+++ src/pages/Offer/Offer.js
@@ -1,66 +1,40 @@
-import React, { useEffect, useState } from "react";
-import SpecialOffers from "../../components/home/SpecialOffers/SpecialOffers";
-import Breadcrumbs from "../../components/pageProps/Breadcrumbs";
-import { useParams } from "react-router-dom";
-import ShopSideNav from "../../components/pageProps/shopPage/ShopSideNav";
-import Brand from "../../components/pageProps/shopPage/shopBy/Brand";
-import Color from "../../components/pageProps/shopPage/shopBy/Color";
-import Category from "../../components/pageProps/shopPage/shopBy/Category";
-import { useDispatch } from "react-redux";
-import { toggleBrand, toggleColor } from "../../redux/orebiSlice";
+import React, { useEffect, useState } from 'react';
+import SpecialOffers from '../../components/home/SpecialOffers/SpecialOffers';
+import Breadcrumbs from '../../components/pageProps/Breadcrumbs';
+import { useParams } from 'react-router-dom';
+import ShopSideNav from '../../components/pageProps/shopPage/ShopSideNav';
+import Brand from '../../components/pageProps/shopPage/shopBy/Brand';
+import Color from '../../components/pageProps/shopPage/shopBy/Color';
+import Category from '../../components/pageProps/shopPage/shopBy/Category';
+import { useDispatch } from 'react-redux';
+import { setFilter, toggleBrand, toggleColor } from '../../redux/orebiSlice';
+import SubCategory from '../../components/pageProps/shopPage/shopBy/subCtegory';
+import ProductBanner from '../../components/pageProps/shopPage/ProductBanner';
 
 const Offer = () => {
-  const [prevLocation] = useState("");
+  const [prevLocation] = useState('');
   const { category } = useParams();
 
   const dispatch = useDispatch();
 
-  // useEffect(() => {
-  //   // Reset filters when component mounts
-  //   dispatch(toggleBrand([])); // Reset brand filter
-  //   dispatch(toggleColor([])); // Reset color filter
-  // }, []);
-
-  // useEffect(() => {
-  //   filterState();
-  // }, []);
-
-  const renderFilters = () => {
-    switch (category) {
-      case "Imprimante":
-        return <Brand />;
-      case "Encre":
-        return <Color />;
-      case "toner":
-        return <Color />;
-      case "Cartouche":
-        return <Color />;
-      // Add more cases for other categories as needed
-      default:
-        return null;
-    }
-  };
-
+  useEffect(() => {
+    dispatch(setFilter());
+  }, [dispatch, category]);
   return (
-    // <div className="max-w-container mx-auto">
-    //   <Breadcrumbs title={category} prevLocation={prevLocation} />
-    //   <div className="pb-10">
-    //     <SpecialOffers />
-    //   </div>
-    // </div>
-
-    <div className="max-w-container mx-auto px-4">
-      <Breadcrumbs title={category} prevLocation={prevLocation} />
-      {/* ================= Products Start here =================== */}
-      <div className="w-full h-full flex pb-20 gap-10">
-        <div className="w-[20%] lgl:w-[25%] hidden mdl:inline-flex h-full">
-          {renderFilters()}
+    <div className="bg-gray-100">
+      <div className="max-w-container mx-auto px-4 ">
+        <Breadcrumbs title={category} prevLocation={prevLocation} />
+        {/* ================= Products Start here =================== */}
+        <div className="w-full h-full flex pb-20 gap-10">
+          <div className="w-[20%] lgl:w-[25%] hidden mdl:inline-flex h-full">
+            <ShopSideNav />
+          </div>
+          <div className="w-full mdl:w-[80%] lgl:w-[75%] h-full flex flex-col gap-10">
+            <SpecialOffers />
+          </div>
         </div>
-        <div className="w-full mdl:w-[80%] lgl:w-[75%] h-full flex flex-col gap-10">
-          <SpecialOffers />
-        </div>
+        {/* ================= Products End here ===================== */}
       </div>
-      {/* ================= Products End here ===================== */}
     </div>
   );
 };
