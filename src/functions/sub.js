cd568f0be6c49af3bf32095e766c740af35f99c3
sofianeFeki <backofficekompar@gmail.com>
2024-09-01 22:12:24 +0100

    Update sub.js

--- src/functions/sub.js
+++ src/functions/sub.js
@@ -1,16 +1,16 @@
 import axios from 'axios';
 
 export const getSubs = async () =>
-  await axios.get('http://localhost:8000/api/subs');
+  await axios.get('https://cic-server-ygl9.onrender.com/api/subs');
 
 export const getSub = async (slug) =>
-  await axios.get(`http://localhost:8000/api/sub/${slug}`);
+  await axios.get(`https://cic-server-ygl9.onrender.com/api/sub/${slug}`);
 
 export const removeSub = async (slug, authtoken) =>
-  await axios.delete(`http://localhost:8000/api/sub/${slug}`, {});
+  await axios.delete(`https://cic-server-ygl9.onrender.com/api/sub/${slug}`, {});
 
 export const updateSub = async (slug, sub, authtoken) =>
-  await axios.put(`http://localhost:8000/api/sub/${slug}`, sub, {});
+  await axios.put(`https://cic-server-ygl9.onrender.com/api/sub/${slug}`, sub, {});
 
 export const createSub = async (sub, authtoken) =>
-  await axios.post(`http://localhost:8000/api/sub`, { sub });
+  await axios.post(`https://cic-server-ygl9.onrender.com/api/sub`, { sub });
