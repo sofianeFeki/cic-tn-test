13cdc7d1e71fc8bec0e87cda3fd355fea975c940
sofianeFeki <backofficekompar@gmail.com>
2024-09-01 22:10:11 +0100

    Update brand.js

--- src/functions/brand.js
+++ src/functions/brand.js
@@ -1,16 +1,16 @@
 import axios from 'axios';
 
 export const getbrands = async () =>
-  await axios.get('http://localhost:8000/api/brands');
+  await axios.get('https://cic-server-ygl9.onrender.com/api/brands');
 
 export const getbrand = async (slug) =>
-  await axios.get(`http://localhost:8000/api/brand/${slug}`);
+  await axios.get(`https://cic-server-ygl9.onrender.com/api/brand/${slug}`);
 
 export const removebrand = async (slug) =>
-  await axios.delete(`http://localhost:8000/api/brand/${slug}`, {});
+  await axios.delete(`https://cic-server-ygl9.onrender.com/api/brand/${slug}`, {});
 
 export const updatebrand = async (slug, brand) =>
-  await axios.put(`http://localhost:8000/api/brand/${slug}`, { brand });
+  await axios.put(`https://cic-server-ygl9.onrender.com/api/brand/${slug}`, { brand });
 
 export const createbrand = async (brand) =>
-  await axios.post(`http://localhost:8000/api/brand`, { brand });
+  await axios.post(`https://cic-server-ygl9.onrender.com/api/brand`, { brand });
