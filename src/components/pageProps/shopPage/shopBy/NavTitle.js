7bfc1c3556ae5f897346a8b49e813632368e15f3
sofianeFeki <backofficekompar@gmail.com>
2024-09-01 21:51:07 +0100

    Update NavTitle.js

--- src/components/pageProps/shopPage/shopBy/NavTitle.js
+++ src/components/pageProps/shopPage/shopBy/NavTitle.js
@@ -1,17 +1,36 @@
-import React from "react";
-import { BiCaretDown } from "react-icons/bi";
+import { PlusIcon } from '@heroicons/react/24/outline';
+import React, { useState } from 'react';
+import { BiCaretDown } from 'react-icons/bi';
+import { useSelector } from 'react-redux';
 
-const NavTitle = ({ title, icons }) => {
+const NavTitle = ({ title, icons, setIsOpen }) => {
+  function openModal() {
+    setIsOpen(true);
+  }
+  const user = useSelector((state) => state.orebi.userInfo);
+
   return (
-    <div className='flex items-center justify-between pb-5'>
+    <div className="flex items-center justify-between pb-5">
       {icons ? (
         <>
-          <h3 className='font-bold lg:text-xl text-primeColor'>{title}</h3>
+          <div className="flex">
+            {' '}
+            <h3 className="font-bold lg:text-xl text-primeColor">{title}</h3>
+            {user && (
+              <button
+                onClick={openModal}
+                className="flex items-center ml-2 px-1 py-1 bg-green-500 text-white rounded cursor-pointer drop-shadow-xl hover:bg-green-300 transition duration-300"
+              >
+                <PlusIcon className="block h-4 w-4 " aria-hidden="true" />
+              </button>
+            )}
+          </div>
+
           {icons && <BiCaretDown />}
         </>
       ) : (
         <>
-          <h3 className='font-bold lg:text-xl text-primeColor'>{title}</h3>
+          <h3 className="font-bold lg:text-xl text-primeColor">{title}</h3>
         </>
       )}
     </div>
